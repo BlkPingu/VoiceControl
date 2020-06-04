387f41fb1d8b66fc5fb90e5e426377cab82aa520
BlkPingu <tobias-kolb@hotmail.de>
2020-06-04 09:57:26 +0200

    add minimal pipline

--- tools/deepspeech/wavTranscriber/main.py
+++ tools/deepspeech/wavTranscriber/main.py
@@ -2,17 +2,22 @@ import deepspeech
 from config import conf
 import wave
 import numpy as np
+import spacy
+
 
 # Run in Bachelorarbeit/Code/DeepSpeech_VirtualEnv:
 # ```
 # python3 voice_control/tools/deepspeech/wavTranscriber/main.py
 # ```
 
+
+nlp = spacy.load("en_core_web_sm")
+model = deepspeech.Model(conf['model_file_path'], conf['beam_width'])
+model.enableDecoderWithLM(conf['lm_file_path'], conf['trie_file_path'], conf['lm_alpha'], conf['lm_beta'])
+
+
+# takes wav
 def transcribe(wav):
-
-    model = deepspeech.Model(conf['model_file_path'], conf['beam_width'])
-
-    model.enableDecoderWithLM(conf['lm_file_path'], conf['trie_file_path'], conf['lm_alpha'], conf['lm_beta'])
 
     w = wave.open(wav, 'r')
 
@@ -21,22 +26,51 @@ def transcribe(wav):
     frames = w.getnframes()
 
     buffer = w.readframes(frames)
-    #print(rate)
-    #print(model.sampleRate())
-
-    type(buffer)
 
     data16 = np.frombuffer(buffer, dtype=np.int16)
 
-    #type(data16)
-
     text = model.stt(data16)
+
     return text
 
+# takes transcribe()
+def process(transcription, keyword):
+    doc = nlp(transcription)
+
+    texts = [token.text for token in doc]
+
+    if keyword in texts:
+        return (transcription, keyword, True)
+    else:
+        return (transcription, keyword, False)
+
+# takes process()
+def print_result(result):
+    prompt = "{t:50s} | {k:10s} | {b:10s}".format(t=result[0], k=result[1], b=str(result[2]))
+    print(prompt)
+
+
+def runner():
+    keyword = "forward"
+
+    transcriptions = [transcribe(dat) for dat in conf['audio_wave_path']]
+    results = [process(transcription, keyword) for transcription in transcriptions]
+
+    print("")
+    print("{t:50s} | {k:10s} | {b:15s}".format(t="input", k="keyword", b="result"))
+    print("-"*75)
+
+    [print_result(result) for result in results]
+    print("")
 
 def main():
-    [print(transcribe(dat)) for dat in conf['audio_wave_path']]
+    runner()
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+

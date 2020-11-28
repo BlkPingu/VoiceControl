d2cfc48eb5c142f03aa884c346721e69261e211d
BlkPingu <tobias-kolb@hotmail.de>
2020-11-28 04:12:31 +0100

    add results

--- src/main/config.py
+++ src/main/config.py
@@ -17,7 +17,7 @@ conf = {
     'csv_path': '/Users/Tobias/Desktop/Bachelorarbeit/Code/VoiceControll/Auswertung/survey_results.csv',
     'uuid_col_name':'UUID',
     'wav_path_col_name':'PATH',
-    'result_csv':'result.csv'
+    'result_csv':'transcription_results.csv'
 }
 
  # test with: survey_results_small.csv
